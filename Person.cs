c6f35615fbdd6265256781a75d346235c118bf6c
siber832 <30592537+siber832@users.noreply.github.com>
2017-08-01 22:27:43 +0200

    Todo

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace sobreescribirOperador {
+    public class Person {
+
+        private String name;
+        private String surname;
+        
+        public Person(String name, String surname) {
+            this.name = name;
+            this.surname = surname;
+        }
+        public Person(String name) : this(name, "") {}
+        
+        public override bool Equals(System.Object ob) {
+            if (ob.GetType() == this.GetType()) {
+                Person p = (Person) ob;
+                return (name == p.name) && (surname == p.surname);
+            } else {
+                return false;
+            }
+        }
+
+        public static bool operator == (Person a, System.Object b) {
+           return Person.Equals(a, b);
+        }
+
+        public static bool operator != (Person a, System.Object b) {
+            return !Person.Equals(a, b);
+        }
+
+        public static String operator + (Person a, Person b) {
+            return String.Concat(a.name, b.name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sobreescribirOperador {
+    class Program {
+        static void Main(string[] args) {
+            Person person1 = new Person("pepe");
+            Person person2 = new Person("juan");
+            Console.WriteLine(person1 == person2);
+            person1 = new Person("juan");
+            Console.WriteLine(person1 == person2);
+            Console.WriteLine(String.Concat("Prueba de suma: ", person1 + person2));
+        }
+    }
+}
